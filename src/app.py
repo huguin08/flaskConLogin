b6d0b5fb9cb0fc96709b6a1f14194e848c76dcf7
huguin08 <humeg1189@gmail.com>
2023-03-23 17:56:09 -0600

    avance de login

--- src/app.py
+++ src/app.py
@@ -1,13 +1,51 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request,redirect, url_for, flash
+from flask_mysqldb import MySQL
+from werkzeug.security import generate_password_hash
 
 from config import config
 
+#model
+from models.ModelUser import ModelUser
+
+#Entities
+from models.entities.User import User
 
 app = Flask(__name__)
 
+db = MySQL(app)
+
+
 @app.route('/')
-def index():
-    return render_template('auth/login.html')
+def index():    
+    return redirect(url_for('login'))
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        # print(request.form['username'])
+        # print(request.form['password'])
+        password_hash = generate_password_hash(request.form['password'])
+        user = User(0,request.form['username'],password_hash)
+        logged_user = ModelUser.login(db,user)
+        if logged_user != None:
+            if logged_user.password:
+                return redirect(url_for('home'))
+            else:
+                flash("Invalid password")
+                return render_template('auth/login.html')
+        else:
+            flash("user not found")
+            return render_template('auth/login.html')
+    else:
+        return render_template('auth/login.html')
+
+@app.route('/home')    
+def home():
+    return render_template('home.html')
+    
+
+
 
 
 if __name__ == '__main__':
--- src/config.py
+++ src/config.py
@@ -1,11 +1,19 @@
 from datetime import timedelta
 
+class Config:
+    SECRET_KEY = 'B!1weNAt1T%kvhUI*S'
+
 
 class ProductionConfig():
     pass
     
-class DevelopmentConfig():
+class DevelopmentConfig(Config):
     DEBUG = True
+    MYSQL_HOST = 'localhost'
+    MYSQL_PORT = 4200
+    MYSQL_USER = 'root'
+    MYSQL_PASSWORD = 'Anakin102089&'
+    MYSQL_DB = 'Codifin'
 
 class TestingConfig():
     TESTING = True
